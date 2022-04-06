9e28e45263d613be5d35bc43fc568aa4b7a750ba
waodeazahra <waodeazahrakhandiah@gmail.com>
2022-04-06 22:13:33 +0700

    Pustaka-bookiing Commit 1

---
+++ pustaka-bookiing/application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Wa Ode Azahra Khandiah Djafar
+ Saya tingga di daerah Sulawesi Tenggara
+ olah raga yang saya sukai adalah
+ Bersepeda";
+ }
+}
---
+++ pustaka-bookiing/application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Wa Ode Azahra</li>
+ <li>Nama Belakang : Khandiah Djafar</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jalan Jendral Sudirman No 72</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Raha</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Bersepeda</li>
+ <li>Bulutangkis</li>
+ </ul>
+</section
